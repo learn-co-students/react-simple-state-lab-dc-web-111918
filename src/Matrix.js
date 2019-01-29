536287fb744a3a463b0aa96bbb558b2c41fc3824
github username <github email address>
2019-01-29 14:51:27 -0500

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -3,7 +3,11 @@ import Cell from './Cell'
 
 class Matrix extends Component {
   genRow = (vals) => (
-    vals.map(val => <Cell value={val}/>) // replace me and render a cell component instead!
+    vals.map(val => <Cell
+        // short hand for invocking an instance of the Cell({value: val}) class with the argument
+        // of props as a { value: val}
+
+      value={val}/>) // replace me and render a cell component instead!
   )
 
   genMatrix = () => (
